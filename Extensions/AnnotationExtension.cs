1a1871e7ccf77153edcdefdbdb14c55abe72a044
Aman Sharma <sharma9917337201@gmail.com>
2023-11-15 21:06:46 +0530

    Validation implemented successfully

---
+++ Extensions/AnnotationExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuyAndSellBike.Extensions
+{
+    public class YearRangeTillNowDate:RangeAttribute
+    {
+        public YearRangeTillNowDate(int startYear):base(startYear, DateTime.Today.Year)
+        {
+
+        }
+    }
+}
--- Models/Bike.cs
+++ Models/Bike.cs
@@ -1,31 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using BuyAndSellBike.Extensions;
 
 namespace BuyAndSellBike.Models
 {
     public class Bike
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Provide Make")]
         public Make Make { get; set; }
+
+        [RegularExpression("^[1-9]*$", ErrorMessage = "Select Model")]
         public int MakeId { get; set; }
+
+        [Required(ErrorMessage ="Provide Model")]
         public Model Model { get; set; }
+
+        [RegularExpression("^[1-9]*$", ErrorMessage ="Select Model")]
         public int ModelId { get; set; }
+
         [Required]
+        [YearRangeTillNowDate(2000, ErrorMessage ="Invalid Year")]
         public int Year { get; set; }
+
         [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "Provide Mileage")]
         public int Mileage { get; set; }
+
         public string features { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="Provide Seller Name")]
         public string SellerName { get; set; }
+
+        [Required(ErrorMessage ="Invalid Email ID")]
+        [DataType(DataType.EmailAddress)]
         public string SellerEmail { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="Provide Seller Phone")]
         public string SellerPhone { get; set; }
-        [Required]
+
+        [Required(ErrorMessage ="Provide Selling Price")]
         public int Price { get; set; }
-        [Required]
+    
+        [Required(ErrorMessage = "Provide Currency")]
         public string Currency { get; set; }
         public string ImagePath { get; set; }
     }
